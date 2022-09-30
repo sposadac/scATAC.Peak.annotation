495c009a0b4c73e7843d6697d9c873ec357e87d2
PatZeis <zeis@ie-freiburg.mpg.de>
2022-09-30 16:30:14 +0200

    harmonize filtering

--- R/scATAC.Peak.annotation.R
+++ R/scATAC.Peak.annotation.R
@@ -702,12 +702,12 @@ give_combined_peaks <- function(atac_layer, filter_reg_chr=F) {
   })
   if (filter_reg_chr==T){
     cat("filter for peaks on regular chromosomes", "\n")
-    atac_layer_peaks_gr <- lapply(atac_layer_peaks, function(x){
+    atac_layer_peaks <- lapply(atac_layer_peaks, function(x){
       y <- x[grepl("^[0-9XYMTCHRxymtchr]+$", x[,1], perl=T),]
     })
   }
   cat("make genomic ranges for peaks of each sample", "\n")
-  atac_layer_peaks_gr <- lapply(atac_layer_peaks_gr, function(x){
+  atac_layer_peaks_gr <- lapply(atac_layer_peaks, function(x){
     y <- makeGRangesFromDataFrame(x)
   })
   cat("reduce peaks of different samples to comman overapping peak set", "\n")
